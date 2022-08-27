ad41c8b1bb25d3f47dd3b3604c71061b73c21324
Amaury Gonçalves Costa <baugoncalves@gmail.com>
2022-08-27 09:38:39 -0300

    changes for invalid options

--- aula4_sabado/calculadora.c
+++ aula4_sabado/calculadora.c
@@ -9,25 +9,40 @@ main() {
     printf("Digite uma opcao: ");
     scanf("%d", &opc);
 
-    printf("Digite o primeiro numero: ");
-    scanf("%d", &num1);
-    printf("Digite o segundo numero: ");
-    scanf("%d", &num2);
-
-    if (opc == 1) {
-        printf("Soma: %d\n", num1+num2);
+    if (opc >=1 && opc <= 6) {
+        printf("Digite um numero: ");
+        scanf("%d", &num1);
+        if (opc != 6) {
+            printf("Digite o segundo numero: ");
+            scanf("%d", &num2);
+        }
+        if (opc == 1) {
+            printf("Soma: %d\n", num1+num2);
+        }
+        if (opc == 2) {
+            printf("Subtracao: %d\n", num1-num2);
+        }
+        if (opc == 3) {
+            printf("Multiplicacao: %d\n", num1*num2);
+        }
+        if (opc == 4) {
+            if (num2 == 0) {
+                printf("Divisao impossivel\n");
+            }
+            else {
+                printf("Divisao: %d\n", num1/num2);
+            }
+        }
+        if (opc == 5) {
+            resultado = pow(num1,num2);
+            printf("Potenciacao: %d\n", resultado);
+        }
+        if (opc == 6) {
+            resultado = sqrt(num1);
+            printf("Raiz quadrada: %d\n", resultado);
+        }
     }
-    if (opc == 2) {
-        printf("Subtracao: %d\n", num1-num2);
-    }
-    if (opc == 3) {
-        printf("Multiplicacao: %d\n", num1*num2);
-    }
-    if (opc == 4) {
-        printf("Divisao: %d\n", num1/num2);
-    }
-    if (opc == 5) {
-        resultado = pow(num1,num2);
-        printf("Potenciacao: %d\n", resultado);
+    else {
+        printf("Opcao invalida\n");
     }
 }
