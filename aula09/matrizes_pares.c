130c2e5fa1fe1b6e6dac06585025d4cdbf5d17f8
Amaury Gonçalves Costa <baugoncalves@gmail.com>
2022-10-05 21:31:08 -0300

    matriz pares

--- aula09/matrizes_pares.c
+++ aula09/matrizes_pares.c
@@ -1,23 +1,38 @@
 #include<stdio.h>
 
 main() {
-    int linha, coluna, l, c, resultado;
+    int qtde_linha, qtde_coluna, l, c, contador = 0;
 
 
     printf("Quantidade de linhas e colunas: ");
-    scanf("%d %d", &linha, &coluna);
+    scanf("%d %d", &qtde_linha, &qtde_coluna);
    
-    int matriz[linha][coluna];
+    int matriz[qtde_linha][qtde_coluna];
 
-    for(l=0; l< linha; l++){
-        for(c=0; c < coluna; c++) {
+    for(l=0; l< qtde_linha; l++){
+        for(c=0; c < qtde_coluna; c++) {
             printf("linha %d coluna %d: ", l+1, c+1);
             scanf("%d", &matriz[l][c]);
-        }
-    }
-    for(linha=0; linha< 3; linha++){
-        for(coluna=0; coluna < 3; coluna++) {
+            if ((matriz[l][c])%2 == 0) {
+                contador++;
+            }
         }
     }
 
+    for(l=0; l< qtde_linha; l++){
+        for(c=0; c < qtde_coluna; c++) {
+            printf("%d\t", matriz[l][c]);
+        }
+        printf("\n");
+    }
+
+    for(l=0; l< qtde_linha; l++){
+        for(c=0; c < qtde_coluna; c++) {
+            if ((matriz[l][c])%2 == 0) {
+                printf("O numero %d e par e esta na linha %d coluna %d\n", matriz[l][c], l+1, c+1);
+                contador++;
+            }
+        }
+    }
+    printf("Ha %d pares na matriz\n", contador);
 }
