5b50b32887b3c9eb98c02778930cce6e23cbffdb
Amaury Gonçalves Costa <baugoncalves@gmail.com>
2022-11-16 21:27:21 -0300

    implement remover

--- aula11/cadastro.c
+++ aula11/cadastro.c
@@ -6,7 +6,7 @@
 main() {
     char nomes[MAX][30], cpf[MAX][11];
     char rua[MAX][50], cep[MAX][8], pesq[11];
-    int opc, i, qtde = 0;
+    int opc, i, qtde = 0, found;
 
     do {
         printf("\n####Cad Doctum###\n\n");
@@ -45,24 +45,50 @@ main() {
                 printf("CPF a pesquisar: ");
                 __fpurge(stdin);
                 gets(pesq);
+                found = 0;
                 for (i=0; i<qtde; i++) {
                     if (strcmp(cpf[i], pesq) == 0) {
                         printf("Nome: %s\n", nomes[i]);
                         printf("CPF: %s\n", cpf[i]);
                         printf("Rua: %s\n", rua[i]);
                         printf("CEP: %s\n\n", cep[i]);
+
+                        found = 1;
                     }
+                }
+                if (found == 0) {
+                    printf("Registro nao encontrado\n");
                 }
             break;
 
             case 4:
-                printf("Remover\n");
+                 printf("CPF a remover: ");
+                __fpurge(stdin);
+                gets(pesq);
+                found = 0;
+                for (i=0; i<qtde; i++) {
+                    if (strcmp(cpf[i], pesq) == 0) {
+                        for (int j = i; j < (qtde-1); j++) {
+                            strcpy(nomes[j], nomes[j+1]);
+                            strcpy(cpf[j], cpf[j+1]);
+                            strcpy(rua[j],rua[j+1]);
+                            strcpy(cep[j],cep[j+1]);
+                        }
+
+                        found = 1;
+                    }
+                }
+                if (found == 0) {
+                    printf("Registro nao encontrado\n");
+                } else {
+                    qtde--;
+                }
             break;
 
             case 5:
                 printf("\033[1;31m");
                 printf("Saindo...\n");
-                sleep(5);
+                sleep(2);
             break;
 
             default:
